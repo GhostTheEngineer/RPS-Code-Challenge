036c90c850dcc617d1bc4950b3aa413a40b40563
quodpipax <quodpipax@mail.ru>
2025-03-21 10:28:22 +0300

    Refactored, more optimized, simpler, secure, faster, no unix support though

--- src/driver.cpp
+++ src/driver.cpp
@@ -3,253 +3,173 @@ import <iostream>;
 import <regex>;
 import <random>;
 import <format>;
+import <conio.h>;
 
 namespace game
 {
 #ifdef _WIN32
-#include <conio.h>      //for _getch();
+
+	void ClearScreen() { system("cls"); }
+	//std::cout << "\033[2J\033[1;1H"; //Fake clear screen - scrolls up to make it seem like an update
+
 #include <windows.h>    //for  SetConsoleTextAttribute(hConsole, int);
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-
-    static inline void ClearScreen()
-    {
-        system("cls");
-    }
-    
 
 #else // UNIX:
-
-    static inline void ClearScreen()
-    {
-        std::system("clear");
-    }
-    std::cout << "\nInstall Windows!\n";
-    exit(EXIT_FAILURE);
+	//void ClearScreen() { std::system("clear"); }
+	std::cout << "\n\nUNIX is not supported!\n\n"
+	exit(EXIT_FAILURE);
 
 #endif
 
 #define KEY_UP 72
 #define KEY_DOWN 80
-#define KEY_LEFT 75
-#define KEY_RIGHT 77
 #define KEY_ENTER 13
 
 
-    const int MIN_ROUNDS{ 1 };
-    const int MAX_ROUNDS{ 100 };
+	const int MIN_ROUNDS{ 1 };
+	const int MAX_ROUNDS{ 100 };
 
-    // Check for integer
-    static bool isInteger(const std::string& str) 
-    {
-        static const std::regex pattern("^-?[0-9]+$");
-        return std::regex_match(str, pattern);
-    }
+	//void ClearScreen();
+	void MenuSelectRounds(int& RoundLimit);
+	int MenuSelectWeapon(const int& round, const int& roundLimit);
+	void MenuWeaponList(const int& round, const int& roundLimit, const int& currentMenuPos);
 
-    void MenuRockSelected();
-    void MenuPaperSelected();
-    void MenuScissorsSelected();
-
-    enum Choice{ Rock=1, Paper, Scissors };
-    enum Color {Black=0, LightGrey=7, DarkGrey=8, Blue=9, Green=10, Cyan=11, Red=12, Pink=13, Yellow=14, White=15};
-
-    bool bRockSelected{ false };
-    bool bPaperSelected{ false };
-    bool bScissorsSelected{ false };
-
-    void run() {
-        //SETUP
-        std::default_random_engine generator;
-        std::uniform_int_distribution<int> distribution(1, 3);
-
-        int playerScore{ 0 }, computerScore{ 0 }, ties{ 0 }, round{ 1 }, roundLimit{ 0 };
-        std::string getUserInput{};
-       
-        //SELECT ROUNDS
-        do{
-            std::cout << std::format("Welcome to Rock, Paper, Scissors!\n");
-            std::cout << std::format("How many rounds would you like to play ({}-{}): ", MIN_ROUNDS, MAX_ROUNDS);
-            std::getline(std::cin, getUserInput);
-
-            if (isInteger(getUserInput)) //Check if integer
-            {
-                int userInput = stoi(getUserInput); //Convert string to int
-                if (MIN_ROUNDS <= userInput && MAX_ROUNDS >= userInput) //Check if in range
-                {
-                    roundLimit = userInput;
-                    break; //If OK break from the loop
-                }
-            }
-            ClearScreen(); //Clear the console screen before repeating the loop
-        } while (true);
-       
-
-        //MAIN GAME
-
-        do { // game loop - top of round
-            ClearScreen();
-            SetConsoleTextAttribute(hConsole, Color::White);
-            std::cout << std::format("\nROUND: {}\\{}\n\n", round, roundLimit);
-           
-            MenuRockSelected(); //Default starting menu
-
-            int userChoice{ 0 };
-            int playerChoice{ 0 };
-
-            while (true)
-            {
-                userChoice = 0;
-                userChoice = _getch();
-
-                if (userChoice == KEY_UP)
-                {
-                    if (bRockSelected)
-                    {
-                        ClearScreen();
-                        SetConsoleTextAttribute(hConsole, Color::White);
-                        std::cout << std::format("\nROUND: {}\\{}\n\n", round, roundLimit);
-                        MenuScissorsSelected();
-                    }
-                    else if (bPaperSelected)
-                    {
-                        ClearScreen();
-                        SetConsoleTextAttribute(hConsole, Color::White);
-                        std::cout << std::format("\nROUND: {}\\{}\n\n", round, roundLimit);
-                        MenuRockSelected();
-                    }
-                    else if (bScissorsSelected)
-                    {
-                        ClearScreen();
-                        SetConsoleTextAttribute(hConsole, Color::White);
-                        std::cout << std::format("\nROUND: {}\\{}\n\n", round, roundLimit);
-                        MenuPaperSelected();
-                    }
-                }
-                if (userChoice == KEY_DOWN)
-                {
-                    if (bRockSelected)
-                    {
-                        ClearScreen();
-                        SetConsoleTextAttribute(hConsole, Color::White);
-                        std::cout << std::format("\nROUND: {}\\{}\n\n", round, roundLimit);
-                        MenuPaperSelected();
-                    }
-                    else if (bPaperSelected)
-                    {
-                        ClearScreen();
-                        SetConsoleTextAttribute(hConsole, Color::White);
-                        std::cout << std::format("\nROUND: {}\\{}\n\n", round, roundLimit);
-                        MenuScissorsSelected();
-                    }
-                    else if (bScissorsSelected)
-                    {
-                        ClearScreen();
-                        SetConsoleTextAttribute(hConsole, Color::White);
-                        std::cout << std::format("\nROUND: {}\\{}\n\n", round, roundLimit);
-                        MenuRockSelected();
-                    }
-                }
-                if (userChoice == KEY_ENTER)
-                {
-                    if (bRockSelected)
-                    {
-                        playerChoice = Choice::Rock;
-                        break;
-                    }
-
-                    if (bPaperSelected)
-                    {
-                        playerChoice = Choice::Paper;
-                        break;
-                    }
-
-                    if (bScissorsSelected)
-                    {
-                        playerChoice = Choice::Scissors;
-                        break;
-                    }
-                }
-            }
+	enum Choice { Rock = 1, Paper, Scissors };
+	enum Color { Black = 0, LightGrey = 7, DarkGrey = 8, Blue = 9, Green = 10, Cyan = 11, Red = 12, Pink = 13, Yellow = 14, White = 15 };
 
 
-            int computerChoice = distribution(generator); 
+	void run() {
+		//SETUP
+		std::default_random_engine generator;
+		std::uniform_int_distribution<int> distribution(1, 3);
 
-            // Display choices
-            SetConsoleTextAttribute(hConsole, Color::LightGrey);
-            std::cout << "\nYou chose: " << (playerChoice == Choice::Rock ? "Rock" : playerChoice == Choice::Paper ? "Paper" : "Scissors") << "\n";
-            std::cout << "Computer chose: " << (computerChoice == Choice::Rock ? "Rock" : computerChoice == Choice::Paper ? "Paper" : "Scissors") << "\n";
+		int playerScore{ 0 }, computerScore{ 0 }, ties{ 0 }, round{ 1 }, roundLimit{ 5 }, userInput{ 0 }, playerChoice{ 0 };
 
-            // Determine the winner of the round
-            if (playerChoice == computerChoice) {
-                std::cout << "It's a tie!\n";
-                ++ties;
-            }
-            else if ((playerChoice == Choice::Rock && computerChoice == Choice::Scissors) ||
-                (playerChoice == Choice::Paper && computerChoice == Choice::Rock) ||
-                (playerChoice == Choice::Scissors && computerChoice == Choice::Paper)) {
-                std::cout << "You win this round!\n";
-                ++playerScore;
-            }
-            else {
-                std::cout << "Computer wins this round!\n";
-                ++computerScore;
-            }
+		//WELCOME SCREEN & SELECT ROUNDS
+		MenuSelectRounds(roundLimit);
 
-            ++round;
+		//MAIN GAME
+		do{
+		playerChoice = MenuSelectWeapon(round, roundLimit);
 
-            std::cout << "\nPress Enter to exit...";
-            std::cin.get();  // Waits for user input before closing terminal
+		int computerChoice = distribution(generator);
 
-        } while (round <= roundLimit);
+		// Display choices
+		SetConsoleTextAttribute(hConsole, Color::LightGrey);
+		std::cout << "\nYou chose: " << (playerChoice == Choice::Rock ? "Rock" : playerChoice == Choice::Paper ? "Paper" : "Scissors") << "\n";
+		std::cout << "Computer chose: " << (computerChoice == Choice::Rock ? "Rock" : computerChoice == Choice::Paper ? "Paper" : "Scissors") << "\n";
 
-        // Display final score
-        std::cout << "\nFinal Score:\n"
-            << "  You [" << playerScore << "]\n"
-            << "  Computer [" << computerScore << "]\n"
-            << "  Ties [" << ties << "]\n"
-            << "Thanks for playing!\n\n";
+		// Determine the winner of the round
+		if (playerChoice == computerChoice) {
+			std::cout << "It's a tie!\n";
+			++ties;
+		}
+		else if ((playerChoice == Choice::Rock && computerChoice == Choice::Scissors) ||
+			(playerChoice == Choice::Paper && computerChoice == Choice::Rock) ||
+			(playerChoice == Choice::Scissors && computerChoice == Choice::Paper)) {
+			std::cout << "You win this round!\n";
+			++playerScore;
+		}
+		else {
+			std::cout << "Computer wins this round!\n";
+			++computerScore;
+		}
 
-        SetConsoleTextAttribute(hConsole, Color::LightGrey);
-        std::cout << "Press Enter to exit...";
-        std::cin.get();  // Waits for user input before closing terminal
-    }
+		++round;
 
-    void MenuRockSelected()
-    {
-        SetConsoleTextAttribute(hConsole, Color::White);
-        std::cout << std::format(">   Rock   <") << std::endl;
-        SetConsoleTextAttribute(hConsole, Color::Blue);
-        std::cout << std::format("   Paper    ") << std::endl;
-        SetConsoleTextAttribute(hConsole, Color::Red);
-        std::cout << std::format("  Scissors  ") << std::endl;
-        bRockSelected = true;
-        bPaperSelected = false;
-        bScissorsSelected = false;
-    }
+		std::cout << "\nPress Enter to continue...";
+		std::cin.get();  // Waits for user input before closing terminal
 
-    void MenuPaperSelected()
-    {
-        SetConsoleTextAttribute(hConsole, Color::White);
-        std::cout << std::format("    Rock    ") << std::endl;
-        SetConsoleTextAttribute(hConsole, Color::Blue);
-        std::cout << std::format(">  Paper   <") << std::endl;
-        SetConsoleTextAttribute(hConsole, Color::Red);
-        std::cout << std::format("  Scissors  ") << std::endl;
-        bRockSelected = false;
-        bPaperSelected = true;
-        bScissorsSelected = false;
-    }
+	} while (round <= roundLimit);
 
-    void MenuScissorsSelected()
-    {
-        SetConsoleTextAttribute(hConsole, Color::White);
-        std::cout << std::format("    Rock    ") << std::endl;
-        SetConsoleTextAttribute(hConsole, Color::Blue);
-        std::cout << std::format("   Paper    ") << std::endl;
-        SetConsoleTextAttribute(hConsole, Color::Red);
-        std::cout << std::format("> Scissors <") << std::endl;
-        bRockSelected = false;
-        bPaperSelected = false;
-        bScissorsSelected = true;
-    }
+	// Display final score
+	std::cout << "\nFinal Score:\n"
+		<< "  You [" << playerScore << "]\n"
+		<< "  Computer [" << computerScore << "]\n"
+		<< "  Ties [" << ties << "]\n"
+		<< "Thanks for playing!\n\n";
+
+	SetConsoleTextAttribute(hConsole, Color::LightGrey);
+	std::cout << "Press Enter to exit...";
+	std::cin.get();  // Waits for user input before closing terminal
 }
+
+void MenuSelectRounds(int& RoundLimit)
+{
+	int userInput{ 0 };
+
+	do {
+		std::cout << "\033[31m" << "Welcome to Rock, Paper, Scissors!\n\n";
+		std::cout << std::format("How many rounds would you like to play ({}-{})?", MIN_ROUNDS, MAX_ROUNDS);
+		std::cout << ("\nUse arrow keys UP and DOWN, then press ENTER.\n\n");
+		std::cout << std::format("Rounds: {}", RoundLimit);
+		userInput = _getch();
+
+		switch (userInput)
+		{
+		case KEY_UP:
+			++RoundLimit;
+			if (MAX_ROUNDS < RoundLimit) RoundLimit = MAX_ROUNDS;
+			break;
+		case KEY_DOWN:
+			--RoundLimit;
+			if (MIN_ROUNDS > RoundLimit) RoundLimit = MIN_ROUNDS;
+			break;
+		default:
+			continue;
+		}
+		ClearScreen(); //Clear the console screen before repeating the loop
+	} while (KEY_ENTER != userInput);
+}
+
+
+int MenuSelectWeapon(const int& round, const int& roundLimit)
+{
+	int userInput{ 0 };
+	int currentMenuPos = Choice::Rock;
+	MenuWeaponList(round, roundLimit, currentMenuPos); //Rock selected by default
+
+	do {
+		userInput = _getch();
+		switch (userInput)
+		{
+		case KEY_UP:
+			--currentMenuPos;
+			if (currentMenuPos == 0) currentMenuPos = 3;
+			break;
+		case KEY_DOWN:
+			++currentMenuPos;
+			if (currentMenuPos == 4) currentMenuPos = 1;
+			break;
+		case KEY_ENTER:
+			break;
+		default:
+			continue;
+		}
+		MenuWeaponList(round, roundLimit, currentMenuPos);
+	} while (KEY_ENTER != userInput);
+
+	return currentMenuPos;
+}
+
+
+void MenuWeaponList(const int& round, const int& roundLimit, const int& currentMenuPos)
+{
+	ClearScreen();
+	SetConsoleTextAttribute(hConsole, Color::White);
+	std::cout << std::format("\nROUND: {}\\{}\n\n", round, roundLimit);
+
+	SetConsoleTextAttribute(hConsole, Color::White);
+	std::cout << std::format("{}   Rock   {}", currentMenuPos==Choice::Rock ? ">" : " ", currentMenuPos == Choice::Rock ? "<" : " ") << std::endl;
+	
+	SetConsoleTextAttribute(hConsole, Color::Blue);
+	std::cout << std::format("{}  Paper   {}", currentMenuPos == Choice::Paper ? ">" : " ", currentMenuPos == Choice::Paper ? "<" : " ") << std::endl;
+	
+	SetConsoleTextAttribute(hConsole, Color::Red);
+	std::cout << std::format("{} Scissors {}", currentMenuPos == Choice::Scissors ? ">" : " ", currentMenuPos == Choice::Scissors ? "<" : " ") << std::endl;
+}
+
+
+} //namespace game
